44db8c6a58281bd60347245be5919ab80559badc
Tu-Nguyen2 <nguyenT7002@hotmail.com>
2024-11-13 14:32:18 -0500

    profile routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'msb', component: MsbComponent},
+  { path: 'profile', component: ProfileComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
